10511d193814a98d6a0503879520b02921c45e89
Wallas9_3 <drame.sissako001@gmail.com>
2019-02-28 23:34:40 +0100

    28/02/2019

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
-console.log('test')
+
+var ecrans = document.querySelectorAll('.displayers');
+
+var ecranDuBas = document.getElementById('bottom-displayer');
+
+var ecranDuHaut = document.getElementById('top-displayer');
+
+var signes = document.querySelectorAll('.signe');
+
+var numeros = document.querySelectorAll('.number');
+
+var effacer = document.getElementById('supp');
+
+const donneeEcranDuBas = [];
+
+function recupInput(item) {
+    donneeEcranDuBas.push(item);
+    ecranDuBas.value = donneeEcranDuBas.join('');
+}
+
+function reset() {
+    ecrans.forEach(element => {
+        element.value = "";
+    })
+}
+
+numeros.forEach(element => {
+    element.addEventListener('click', function() {
+       let number =  +element.innerHTML;
+       recupInput(number);
+        console.log(number); 
+    })
+});
+
+signes.forEach(element => {
+    element.addEventListener('click', function() {
+       let signe = element.innerHTML;
+       recupInput(signe);
+        console.log(signe); 
+    })
+});
+
+effacer.addEventListener('click', function() {
+    reset();
+    console.log('AC')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const uglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV === "dev";
 
-module.exports = {
+
+ let config = {
+  mode:"development",
+  watch: true,
   entry: './src/main.js',
+  watch: true,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+
+    ]
+  },
+  plugins: [
+
+  ]
 };
+
+if(!dev) {
+  config.plugins.push(new uglifyJSPlugin())
+}
+
+module.exports = config;
