e88176b8682d33aaa9de5c8bd68f3696390d0e50
Wallas9_3 <drame.sissako001@gmail.com>
2019-03-02 15:59:33 +0100

    fonction calcualate 1.0

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { isNumber } from "util";
 
 var ecrans = document.querySelectorAll('.displayers');
 
@@ -15,24 +16,73 @@ var donneeEcranDuBas = [];
 
 var donneeEcranDuHaut = [];
 
+var resultats = []
+
 ecranDuBas.value = "";
 
 ecranDuHaut.value = "";
+
+function calculate() {
+    let resultat = 0;
+    let finalTab = [...donneeEcranDuHaut];
+
+    finalTab.pop(); // retirer le signe "="
+    console.log(finalTab)
+    for(let i = 0; i <= finalTab.length; i++){
+        
+            if(isNaN(finalTab[i])) {
+                continue;
+                
+            }else {
+                switch (finalTab[i-1]) {
+                    case '+':
+                        console.log('addition')
+                        resultat += parseInt(finalTab[i]);
+                        console.log(resultat)
+                        break;
+                    case '-':
+                        console.log('soustraction')
+                        console.log(finalTab[i])
+                        resultat -= parseInt(finalTab[i]);
+                        break;
+                    case '*':
+                        console.log('multiplication')
+                        console.log(finalTab[i])
+                        resultat *= parseInt(finalTab[i]);
+                        break;
+                    case '÷':
+                        console.log('division')
+                        console.log(finalTab[i])
+                        resultat /= parseInt(finalTab[i]);
+                        break;
+                    default:
+                        resultat += parseInt(finalTab[i]);
+                        break;
+                }
+            }
+    }
+    return resultat;
+}
 
 
 function recupInput(input) {
     if(isNaN(input)) {
         if(input === "=") {
-            var result = calculate();
-            ecranDuBas.value = result;
-            donneeEcranDuHaut.push(input, resultat);
+            donneeEcranDuHaut.push(...donneeEcranDuBas,input);
+            let resultat = calculate();
+            resultats.push(resultat);
+            donneeEcranDuHaut.push(resultat);
             ecranDuHaut.value = donneeEcranDuHaut.join('');
+            donneeEcranDuHaut = [resultat]
+            donneeEcranDuBas = []
+            ecranDuBas.value = resultat;
+            console.log(...donneeEcranDuHaut)
         }else {
             ecranDuBas.value = input;
             donneeEcranDuHaut.push(...donneeEcranDuBas, input);
             ecranDuHaut.value = donneeEcranDuHaut.join('');
+            console.log(...donneeEcranDuHaut)
             donneeEcranDuBas = [];
-            console.log(donneeEcranDuBas);
         }
         
     }else {
@@ -42,6 +92,9 @@ function recupInput(input) {
 }
 
 function reset() {
+    donneeEcranDuHaut = [];
+    donneeEcranDuBas = [];
+    resultats = [];
     ecrans.forEach(element => {
         element.value = "";
     })
